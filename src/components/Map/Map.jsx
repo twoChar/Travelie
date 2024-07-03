b0613693fe6df61579f2d7e39f38e9b2f9cde878
Tushar Suredia <tucy254@gmail.com>
2024-07-03 09:09:14 +0530

    Done for instance

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,7 +4,7 @@ import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
 import Rating from '@material-ui/lab/Rating';
 
-// import mapStyles from '../../mapStyles';
+import mapStyles from './mapStyles';
 import useStyles from './styles';
 
 const Map = ({ setCoordinates, setBounds, coordinates,places, setChildClicked }) => {
@@ -20,7 +20,7 @@ const Map = ({ setCoordinates, setBounds, coordinates,places, setChildClicked })
                 center={coordinates}
                 defaultZoom={14}
                 margin={[50, 50, 50, 50]}
-                options={''}
+                options={{disableDefaultUI: true, zoomControl: true, styles: mapStyles }}
                 onChange={(e) => {
                     setCoordinates({ lat: e.center.lat, lng: e.center.lng });
                     setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
